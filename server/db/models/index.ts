a8250bf37c09ae2430b30242fe7a2c168bbb641a
SeqSEE <sarah@sqdmc.net>
2022-12-07 15:06:10 -0500

    remove unused models

--- server/db/models/index.ts
+++ server/db/models/index.ts
@@ -1,7 +1,1 @@
-import { Account } from './Account';
-import { Auth } from './Auth';
-import { Game } from './Game';
-import { GameStat } from './GameStat';
-import { GameType } from './GameType';
-
-export { Account, Auth, Game, GameStat, GameType };
+//Use this to export all models as one object
