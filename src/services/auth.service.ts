300b0c762dfa497c33c8199dc362a5c8ef627d9e
SeqSEE <SeqSEE@users.noreply.github.com>
2023-02-09 18:39:43 -0500

    removed unused ethereum parts

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,12 +5,10 @@ import { fetchWrapper } from '@src/helpers/FetchWrapper';
 export const authService = {
   fetchNonce,
   verifyMessage,
-  validateTokenETH,
   validateTokenMRX
 };
 
 const baseUrl = apiUrl != undefined ? `${apiUrl}/auth` : `${extApiUrl}/auth`;
-const baseUrl2 = apiUrl != undefined ? `${apiUrl}/auth2` : `${extApiUrl}/auth2`;
 
 function fetchNonce(account: string): Promise<any> {
   return fetchWrapper.get(`${baseUrl}/signature/${account}`);
@@ -23,10 +21,6 @@ function verifyMessage(params: {
   return fetchWrapper.post(`${baseUrl}/signature/`, params);
 }
 
-async function validateTokenETH(params: { cookie: string }): Promise<any> {
-  return await fetchWrapper.post(`${baseUrl}/token`, params);
-}
-
 async function validateTokenMRX(params: { cookie: string }): Promise<any> {
-  return await fetchWrapper.post(`${baseUrl2}/signature/validate`, params);
+  return await fetchWrapper.post(`${baseUrl}/signature/validate`, params);
 }
